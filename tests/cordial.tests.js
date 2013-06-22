cb7fc9dd97e71d66e07057cef617f6c6ed1ed70f
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2013-06-22 16:50:24 -0500

    Adding module/setup/teardown to tests

--- tests/cordial.tests.js
+++ tests/cordial.tests.js
@@ -2,6 +2,19 @@
  * cordial.tests.js: QUnit tests for Cordial.js.
  */
 
+var sandbox;
+var oldCordial = Cordial;
+
+module('Cordial', {
+    setup: function () {
+        sandbox = sinon.sandbox.create();
+    },
+    teardown: function () {
+        sandbox.restore();
+        Cordial = oldCordial;
+    }
+});
+
 test('Cordial.noConflict() resets Cordial object and returns itself', 2, function () {
     // arrange
     var currentCordial = Cordial;
